1b1da02f056b767566fbb43c54472f1cf268d46c
roopeh <44685964+roopeh@users.noreply.github.com>
2018-10-16 14:32:12 +0300

    Update to Java 1.8

--- android/app/src/main/java/com/example/user/currencyconverter/MainActivity.kt
+++ android/app/src/main/java/com/example/user/currencyconverter/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.user.currencyconverter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
